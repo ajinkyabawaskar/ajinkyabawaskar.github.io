1f61c86b392c3e5e82faf56f7d41afecfb998c3c
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-05-01 21:21:03 +0530

    wip

--- index.php
+++ index.php
@@ -161,7 +161,8 @@
         <div class="container">
             <span class="text-muted">
                 <?php
-                echo  preg_replace('<address>', '', trim("Page loaded in " . number_format(microtime(true) - $starttime, 2) . "s running " . (strval($_SERVER['SERVER_SIGNATURE']))))." OK".$_SERVER['HTTP_X_FORWARDED_FOR'];
+                echo  preg_replace('<address>', '', trim("Page loaded in " . number_format(microtime(true) - $starttime, 2) . "s running " . (strval($_SERVER['SERVER_SOFTWARE']))));
+                echo '<script>console.log("If you are reading this, I will read this: "'.$_SERVER['HTTP_X_FORWARDED_FOR'].');</script>';
                 ?></span>
         </div>
     </footer>
