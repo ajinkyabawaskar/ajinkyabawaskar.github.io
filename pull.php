4cab3f3d4cbce0ce2e1981e98b76446e27e61562
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-05-02 23:53:57 +0530

    wip

--- pull.php
+++ pull.php
@@ -1,5 +1,7 @@
 <?php
 if ( $_POST['payload'] ) {
-    shell_exec( ‘cd /var/www/ajinkya.space/ && sudo git reset –hard HEAD && sudo git pull’ );
+    shell_exec( "cd /var/www/ajinkya.space/ && sudo git reset –hard HEAD && sudo git pull" );
+} else {
+    header("Location: index.php");
 }
-?>hi
+?>
