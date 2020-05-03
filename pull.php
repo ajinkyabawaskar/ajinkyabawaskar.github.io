6db14e9fd362208290d2e8ce1663bff0bd9df6b6
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-05-03 12:00:53 +0530

    wip

--- pull.php
+++ pull.php
@@ -6,7 +6,7 @@ case 'POST':
 	header('Content-Type: application/json');
 	$logfile = fopen("log.json", "w") or die("Unable to open file!");
 	$json = $_POST['payload'];
-	shell_exec( ‘cd /var/www/ajinkya.space/ && git reset –hard HEAD && git pull’ );
+	shell_exec( ‘cd /var/www/ajinkya.space/ && sudo git reset –hard HEAD && sudo git pull’ );
 	fwrite($logfile, $json);
 	fclose($logfile);
 	header('HTTP/1.0 200 OK', true, 200);
