48190ede595e105e0662e882853121c0bda7b6c1
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-05-21 18:53:17 +0530

    wip

--- index.php
+++ index.php
@@ -614,16 +614,16 @@ if (isset($_POST['contactForm'])) {
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <?php
-    $date = date("h:i:sa - d M Y, D");
-    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-    $page_load =  number_format(microtime(true) - $starttime, 2);
-    $server = ($_SERVER['SERVER_SOFTWARE']);
-    $data = compact("date", "ip", "page_load", "server");
-    echo '<script>console.log("%c"+' . json_encode($data) .'"+ ", "position: absolute; top: 10px; padding:5px; font-weight: 400; font-family: Roboto; font-size: 20px; margin:0 auto; align:center;");</script>';
-    $log = fopen("log.json", "r") or die("Unable to open file!");
-    $json = fread($log, filesize("log.json"));
-    echo "<script>console.log(" . $json . ".after.substr(0, 7))</script>";
-    fclose($log);
+    // $date = date("h:i:sa - d M Y, D");
+    // $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    // $page_load =  number_format(microtime(true) - $starttime, 2);
+    // $server = ($_SERVER['SERVER_SOFTWARE']);
+    // $data = compact("date", "ip", "page_load", "server");
+    // echo '<script>console.log("%c"+' . json_encode($data) .'"+ ", "position: absolute; top: 10px; padding:5px; font-weight: 400; font-family: Roboto; font-size: 20px; margin:0 auto; align:center;");</script>';
+    // $log = fopen("log.json", "r") or die("Unable to open file!");
+    // $json = fread($log, filesize("log.json"));
+    // echo "<script>console.log(" . $json . ".after.substr(0, 7))</script>";
+    // fclose($log);
     ?>
     <script src="script.js"></script>
 </body>
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("%cajinkya.space", "position: absolute; top: 10px; padding:5px; font-weight: 400; font-family: Roboto; font-size: 20px; margin:0 auto; align:center;");
-console.log("%cUbuntu / Apache 2", "position: absolute; top: 10px; padding:5px; font-weight: 400; font-family: Roboto; font-size: 15px; margin:0 auto; align:center;");
+console.log("%cApache\/2.4.29 (Ubuntu)", "position: absolute; top: 10px; padding:5px; font-weight: 400; font-family: Roboto; font-size: 15px; margin:0 auto; align:center;");
 
 function goToConnect(el) {
 
